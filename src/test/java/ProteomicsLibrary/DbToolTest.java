bd95f511a2b7d60afae1c02d51ffb67ee4cfbffc
Fengchao <fyuab@connect.ust.hk>
2018-03-13 18:56:12 +0800

    Add a test.

--- src/test/java/ProteomicsLibrary/DbToolTest.java
+++ src/test/java/ProteomicsLibrary/DbToolTest.java
@@ -3,8 +3,7 @@ package ProteomicsLibrary;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 import static org.junit.Assert.assertEquals;
 
@@ -17,7 +16,7 @@ public class DbToolTest {
     }
 
     @Test
-    public void returnSeqMap() throws Exception {
+    public void returnSeqMap() {
         Map<String, String> pro_seq_map = db_tool_obj.getProteinSequenceMap();
         Map<String, String> ground_truth = new HashMap<>();
         ground_truth.put("Pro1 test protein one", "ASRIATAAAASKPSLNKF");
@@ -27,7 +26,22 @@ public class DbToolTest {
     }
 
     @Test
-    public void returnAnnotateMap() throws Exception {
+    public void findPeptideLocation() {
+        Set<Integer> result = DbTool.findPeptideLocation("MAGSYFCDSKCKLRCSKAGLADRCLKYCGICCEECKCVPSGTYGNKHECPCYRDKKNSKGKSKCP*", "nM(-2.946)A(26.016)GS(57.021)YFCDSKc", "KR", "P");
+        Set<Integer> groundTruth = new HashSet<>();
+        groundTruth.add(0);
+        assertEquals(result.size(), groundTruth.size());
+        Integer[] resultArray = result.toArray(new Integer[result.size()]);
+        Arrays.sort(resultArray);
+        Integer[] groundTruthArray = groundTruth.toArray(new Integer[groundTruth.size()]);
+        Arrays.sort(groundTruthArray);
+        for (int i = 0; i < resultArray.length; ++i) {
+            assertEquals(resultArray[i], groundTruthArray[i]);
+        }
+    }
+
+    @Test
+    public void returnAnnotateMap() {
         Map<String, String> pro_annotate_map = db_tool_obj.getProteinAnnotateMap();
         Map<String, String> ground_truth = new HashMap<>();
         ground_truth.put("Pro1 test protein one", "Pro1 test protein one");
