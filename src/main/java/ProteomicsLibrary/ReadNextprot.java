b78245161f3324a0ae0443f391daa7aa85cbc50f
Fengchao <fyuab@connect.ust.hk>
2018-04-02 17:28:44 +0800

    Consider Mod other than PSI mod. Fix bugs.

--- src/main/java/ProteomicsLibrary/ReadNextprot.java
+++ src/main/java/ProteomicsLibrary/ReadNextprot.java
@@ -12,7 +12,9 @@ public class ReadNextprot {
     private static final Pattern variantPattern = Pattern.compile("\\\\VariantSimple=([^=\\\\ ]+)");
     private static final Pattern variantPattern2 = Pattern.compile("([0-9]+)\\|([A-Z*])");
     private static final Pattern psiModPattern = Pattern.compile("\\\\ModResPsi=([^=\\\\]+)");
-    private static final Pattern psiModPattern2 = Pattern.compile("([0-9]+)\\|(MOD:[^|]+)");
+    private static final Pattern psiModPattern2 = Pattern.compile("([0-9]+)\\|(MOD:[^|()]+)");
+    private static final Pattern modPattern = Pattern.compile("\\\\ModRes=([^=\\\\]+)");
+    private static final Pattern modPattern2 = Pattern.compile("([0-9]+)\\|\\|([^|()]+)");
 
     private Map<String, Entry> idEntryMap = new HashMap<>();
 
@@ -23,8 +25,10 @@ public class ReadNextprot {
         for (String id : proteinAnnotationMap.keySet()) {
             Matcher variantMatcher = variantPattern.matcher(proteinAnnotationMap.get(id));
             Matcher psiModMatcher = psiModPattern.matcher(proteinAnnotationMap.get(id));
+            Matcher modMatcher = modPattern.matcher(proteinAnnotationMap.get(id));
             Multimap<Integer, Character> locationVariantMap = HashMultimap.create();
             Multimap<Integer, String> locationPsiModMap = HashMultimap.create();
+            Multimap<Integer, String> locationModMap = HashMultimap.create();
             if (variantMatcher.find()) {
                 String tempStr = variantMatcher.group(1);
                 Matcher variantMatcher2 = variantPattern2.matcher(tempStr);
@@ -36,7 +40,8 @@ public class ReadNextprot {
                     char aa2 = variantMatcher2.group(2).trim().charAt(0);
                     locationVariantMap.put(location, aa2);
                 }
-            } else if (psiModMatcher.find()) {
+            }
+            if (psiModMatcher.find()) {
                 String tempStr = psiModMatcher.group(1);
                 Matcher psiModMatcher2 = psiModPattern2.matcher(tempStr);
                 while (psiModMatcher2.find()) {
@@ -45,11 +50,20 @@ public class ReadNextprot {
                     locationPsiModMap.put(location, psiMod);
                 }
             }
+            if (modMatcher.find()) {
+                String tempSet = modMatcher.group(1);
+                Matcher modMatcher2 = modPattern2.matcher(tempSet);
+                while (modMatcher2.find()) {
+                    int location = Integer.valueOf(modMatcher2.group(1).trim());
+                    String mod = modMatcher2.group(2).trim();
+                    locationModMap.put(location, mod);
+                }
+            }
             if (!locationVariantMap.isEmpty() || !locationPsiModMap.isEmpty()) {
                 if (idEntryMap.containsKey(id)) {
                     throw new Exception(String.format(Locale.US, "There are duplicate protein IDs: %s.", id));
                 }
-                idEntryMap.put(id, new Entry(id, proteinSequenceMap.get(id), locationVariantMap, locationPsiModMap));
+                idEntryMap.put(id, new Entry(id, proteinSequenceMap.get(id), locationVariantMap, locationModMap, locationPsiModMap));
             }
         }
     }
@@ -64,12 +78,14 @@ public class ReadNextprot {
         public final String sequence;
         public final Multimap<Integer, Character> locationVariantMap;
         public final Multimap<Integer, String> locationModMap;
+        public final Multimap<Integer, String> locationPsiModMap;
 
-        public Entry(String id, String sequence, Multimap<Integer, Character> locationVariantMap, Multimap<Integer, String> locationModMap) {
+        public Entry(String id, String sequence, Multimap<Integer, Character> locationVariantMap, Multimap<Integer, String> locationModMap, Multimap<Integer, String> locationPsiModMap) {
             this.id = id;
             this.sequence = sequence;
             this.locationVariantMap = locationVariantMap;
             this.locationModMap = locationModMap;
+            this.locationPsiModMap = locationPsiModMap;
         }
     }
 }
