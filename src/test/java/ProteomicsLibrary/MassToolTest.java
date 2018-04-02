d7380fe709dbe96a93e8ab0c2584c70558dc599f
Fengchao <fyuab@connect.ust.hk>
2018-04-02 17:26:19 +0800

    Change some variables' names. Fix some bugs and do some improvements.

--- src/test/java/ProteomicsLibrary/MassToolTest.java
+++ src/test/java/ProteomicsLibrary/MassToolTest.java
@@ -5,108 +5,107 @@ import org.junit.Test;
 import ProteomicsLibrary.Types.*;
 
 import java.util.*;
+import java.util.regex.Pattern;
 
 import static org.junit.Assert.*;
 
 
 public class MassToolTest {
 
-    private static Map<Character, Double> fix_mod_map = new HashMap<>();
+    private static Map<Character, Double> fixModMap = new HashMap<>();
 
     @Before
-    public void setUp() throws Exception {
-        fix_mod_map.put('G', 0d);
-        fix_mod_map.put('A', 0d);
-        fix_mod_map.put('S', 0d);
-        fix_mod_map.put('P', 0d);
-        fix_mod_map.put('V', 0d);
-        fix_mod_map.put('T', 0d);
-        fix_mod_map.put('C', 57.02146);
-        fix_mod_map.put('I', 0d);
-        fix_mod_map.put('L', 0d);
-        fix_mod_map.put('N', 0d);
-        fix_mod_map.put('D', 0d);
-        fix_mod_map.put('Q', 0d);
-        fix_mod_map.put('K', 0d);
-        fix_mod_map.put('E', 0d);
-        fix_mod_map.put('M', 0d);
-        fix_mod_map.put('H', 0d);
-        fix_mod_map.put('F', 0d);
-        fix_mod_map.put('R', 0d);
-        fix_mod_map.put('Y', 0d);
-        fix_mod_map.put('W', 0d);
-        fix_mod_map.put('U', 0d);
-        fix_mod_map.put('O', 0d);
-        fix_mod_map.put('n', 60d);
-        fix_mod_map.put('c', 10d);
+    public void setUp() {
+        fixModMap.put('G', 0d);
+        fixModMap.put('A', 0d);
+        fixModMap.put('S', 0d);
+        fixModMap.put('P', 0d);
+        fixModMap.put('V', 0d);
+        fixModMap.put('T', 0d);
+        fixModMap.put('C', 57.02146);
+        fixModMap.put('I', 0d);
+        fixModMap.put('L', 0d);
+        fixModMap.put('N', 0d);
+        fixModMap.put('D', 0d);
+        fixModMap.put('Q', 0d);
+        fixModMap.put('K', 0d);
+        fixModMap.put('E', 0d);
+        fixModMap.put('M', 0d);
+        fixModMap.put('H', 0d);
+        fixModMap.put('F', 0d);
+        fixModMap.put('R', 0d);
+        fixModMap.put('Y', 0d);
+        fixModMap.put('W', 0d);
+        fixModMap.put('U', 0d);
+        fixModMap.put('O', 0d);
+        fixModMap.put('n', 60d);
+        fixModMap.put('c', 10d);
     }
 
     @Test
-    public void calResidueMass() throws Exception {
-        MassTool mass_tool_obj = new MassTool(1, fix_mod_map, "KR", "P", true, 1.0005 * 0.5, 0.6, "N14", "()");
-        assertEquals(2503.1357421875, mass_tool_obj.calResidueMass("nGASPVTCILNDQKEMHFRYWc"), 0.001);
+    public void calResidueMass() {
+        MassTool massTool = new MassTool(1, fixModMap, "KR", "P", true, 1.0005 * 0.5, 0.6, "N14", "()");
+        assertEquals(2503.1357421875, massTool.calResidueMass("nGASPVTCILNDQKEMHFRYWc"), 0.001);
     }
 
     @Test
-    public void mzToBin() throws Exception {
-        MassTool mass_tool_obj = new MassTool(1, fix_mod_map, "KR", "P", true, 1.0005 * 0.5, 0.6, "N14", "()");
-        assertEquals(11, mass_tool_obj.mzToBin(11), 1e-6);
-        assertEquals(0, mass_tool_obj.mzToBin(0), 1e-6);
-        assertEquals(-5, mass_tool_obj.mzToBin(-5), 1e-6);
     }
 
     @Test
-    public void buildChainSet() throws Exception {
+    public void mzToBin() {
+        MassTool massTool = new MassTool(1, fixModMap, "KR", "P", true, 1.0005 * 0.5, 0.6, "N14", "()");
+        assertEquals(11, massTool.mzToBin(11), 1e-6);
+        assertEquals(0, massTool.mzToBin(0), 1e-6);
+        assertEquals(-5, massTool.mzToBin(-5), 1e-6);
+    }
+
+    @Test
+    public void buildChainSet() {
         // 1 missed-cleavage, N-term linkable
-        MassTool mass_tool_obj = new MassTool(1, fix_mod_map, "KR", "P", true, 1.0005 * 0.5, 0.6, "N14", "()");
-        Set<String> result = mass_tool_obj.buildChainSet("MRGFASSASRIATAAAASKPSLNASTSVNPKLSKTMDYMRIFSVFVVTLWIIRVDARVFKTY", (short) 1);
-        Set<String> ground_truth = new HashSet<>();
-        ground_truth.add("nMRc");
-        ground_truth.add("nRc");
-        ground_truth.add("nMRGFASSASRc");
-        ground_truth.add("nRGFASSASRc");
-        ground_truth.add("nGFASSASRIATAAAASKPSLNASTSVNPKc");
-        ground_truth.add("nIATAAAASKPSLNASTSVNPKc");
-        ground_truth.add("nIATAAAASKPSLNASTSVNPKLSKc");
-        ground_truth.add("nLSKTMDYMRc");
-        ground_truth.add("nVFKTYc");
+        MassTool massTool = new MassTool(1, fixModMap, "KR", "P", true, 1.0005 * 0.5, 0.6, "N14", "()");
+        Set<String> result = massTool.buildChainSet("MRGFASSASRIATAAAASKPSLNASTSVNPKLSKTMDYMRIFSVFVVTLWIIRVDARVFKTY", (short) 1);
+        Set<String> groundTruth = new HashSet<>();
+        groundTruth.add("nMRc");
+        groundTruth.add("nRc");
+        groundTruth.add("nMRGFASSASRc");
+        groundTruth.add("nRGFASSASRc");
+        groundTruth.add("nGFASSASRIATAAAASKPSLNASTSVNPKc");
+        groundTruth.add("nIATAAAASKPSLNASTSVNPKc");
+        groundTruth.add("nIATAAAASKPSLNASTSVNPKLSKc");
+        groundTruth.add("nLSKTMDYMRc");
+        groundTruth.add("nVFKTYc");
 
-        String[] groundTruthArray = ground_truth.toArray(new String[0]);
+        String[] groundTruthArray = groundTruth.toArray(new String[0]);
         Arrays.sort(groundTruthArray);
         String[] resultArray = result.toArray(new String[0]);
         Arrays.sort(resultArray);
         assertArrayEquals(groundTruthArray, resultArray);
 
         // 2 missed-cleavage, N-term linkable
-        mass_tool_obj = new MassTool(2, fix_mod_map, "KR", "P", true, 1.0005 * 0.5, 0.6, "N14", "()");
-        result = mass_tool_obj.buildChainSet("MRGFASSASRIATAAAASKPSLNASTSVNPKLSKTMDYMRIFSVFVVTLWIIRVDARVFKTY", (short) 1);
-        ground_truth = new HashSet<>();
-        ground_truth.add("nMRc");
-        ground_truth.add("nRc");
-        ground_truth.add("nIATAAAASKPSLNASTSVNPKc");
-        ground_truth.add("nMRGFASSASRc");
-        ground_truth.add("nRGFASSASRc");
-        ground_truth.add("nGFASSASRIATAAAASKPSLNASTSVNPKc");
-        ground_truth.add("nIATAAAASKPSLNASTSVNPKLSKc");
-        ground_truth.add("nLSKTMDYMRc");
-        ground_truth.add("nVFKTYc");
-        ground_truth.add("nMRGFASSASRIATAAAASKPSLNASTSVNPKc");
-        ground_truth.add("nRGFASSASRIATAAAASKPSLNASTSVNPKc");
-        ground_truth.add("nGFASSASRIATAAAASKPSLNASTSVNPKLSKc");
-        ground_truth.add("nIATAAAASKPSLNASTSVNPKLSKTMDYMRc");
-        ground_truth.add("nLSKTMDYMRIFSVFVVTLWIIRc");
-        ground_truth.add("nVDARVFKTYc");
+        massTool = new MassTool(2, fixModMap, "KR", "P", true, 1.0005 * 0.5, 0.6, "N14", "()");
+        result = massTool.buildChainSet("MRGFASSASRIATAAAASKPSLNASTSVNPKLSKTMDYMRIFSVFVVTLWIIRVDARVFKTY", (short) 1);
+        groundTruth = new HashSet<>();
+        groundTruth.add("nMRc");
+        groundTruth.add("nRc");
+        groundTruth.add("nIATAAAASKPSLNASTSVNPKc");
+        groundTruth.add("nMRGFASSASRc");
+        groundTruth.add("nRGFASSASRc");
+        groundTruth.add("nGFASSASRIATAAAASKPSLNASTSVNPKc");
+        groundTruth.add("nIATAAAASKPSLNASTSVNPKLSKc");
+        groundTruth.add("nLSKTMDYMRc");
+        groundTruth.add("nVFKTYc");
+        groundTruth.add("nMRGFASSASRIATAAAASKPSLNASTSVNPKc");
+        groundTruth.add("nRGFASSASRIATAAAASKPSLNASTSVNPKc");
+        groundTruth.add("nGFASSASRIATAAAASKPSLNASTSVNPKLSKc");
+        groundTruth.add("nIATAAAASKPSLNASTSVNPKLSKTMDYMRc");
+        groundTruth.add("nLSKTMDYMRIFSVFVVTLWIIRc");
+        groundTruth.add("nVDARVFKTYc");
 
-        groundTruthArray = ground_truth.toArray(new String[0]);
+        groundTruthArray = groundTruth.toArray(new String[0]);
         Arrays.sort(groundTruthArray);
         resultArray = result.toArray(new String[0]);
         Arrays.sort(resultArray);
         assertArrayEquals(groundTruthArray, resultArray);
-    }
-
-    @Test
-    public void buildTheoVector() throws Exception { // todo: complete
-
     }
 
     @Test
@@ -143,15 +142,14 @@ public class MassToolTest {
 
     @Test
     public void seqToAAList() {
-        MassTool mass_tool_obj = new MassTool(1, fix_mod_map, "KR", "P", true, 1.0005 * 0.5, 0.6, "N14", "()");
         String seq = "nGHUKc";
         AA[] result = MassTool.seqToAAList(seq, "[]");
-        AA[] ground_truth = new AA[]{new AA('n', 0), new AA('G', 0), new AA('H', 0), new AA('U', 0), new AA('K', 0), new AA('c', 0)};
-        assertArrayEquals(ground_truth, result);
+        AA[] groundTruth = new AA[]{new AA('n', 0), new AA('G', 0), new AA('H', 0), new AA('U', 0), new AA('K', 0), new AA('c', 0)};
+        assertArrayEquals(groundTruth, result);
 
         seq = "nGH[3.02]UKc";
         result = MassTool.seqToAAList(seq, "[]");
-        ground_truth = new AA[]{new AA('n', 0), new AA('G', 0), new AA('H', 3.02), new AA('U', 0), new AA('K', 0), new AA('c', 0)};
-        assertArrayEquals(ground_truth, result);
+        groundTruth = new AA[]{new AA('n', 0), new AA('G', 0), new AA('H', 3.02), new AA('U', 0), new AA('K', 0), new AA('c', 0)};
+        assertArrayEquals(groundTruth, result);
     }
 }
